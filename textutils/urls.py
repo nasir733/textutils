c2f80e0ca3731d3fd94f0fff910463d4ab00c7bf
nasir <engnasir733@gmail.com>
2020-07-04 09:20:56 +0500

    added templatest

--- textutils/urls.py
+++ textutils/urls.py
@@ -17,8 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
+# Code for video 7
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('removepunc', views.removepunc, name='rempun'),
+    path('capitalizefirst', views.capfirst, name='capfirst'),
+    path('newlineremove', views.newlineremove, name='newlineremove'),
+    path('spaceremove', views.spaceremove, name='spaceremove'),
+    path('charcount', views.charcount, name='charcount'),
+
 ]
--- textutils/views.py
+++ textutils/views.py
@@ -1,9 +1,24 @@
 # i have created this website -Nasir
 from django.http import HttpResponse
+from django.shortcuts import render
 
-# Code for video: 6
+# Code for video 7
+
 def index(request):
-    return HttpResponse('''nasir  Django NasirIqbal''')
+    print(request.GET.get('text','default'))
+    return render(request, 'index.html')
+def removepunc(request):
+    return HttpResponse("remove punc")
 
-def about(request):
-    return HttpResponse("About Nasir")
+def capfirst(request):
+    return HttpResponse("capitalize first")
+
+def newlineremove(request):
+    return HttpResponse("capitalize first")
+
+
+def spaceremove(request):
+    return HttpResponse("space remover")
+
+def charcount(request):
+    return HttpResponse("charcount ")
